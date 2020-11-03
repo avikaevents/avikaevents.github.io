06ac13bb5020dedf5c33f8ba5153ce71a73cde3c
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-11-04 00:54:27 +0530

    trying and testing

--- js/constant.js
+++ js/constant.js
@@ -1,7 +1,8 @@
-export const organization={
+export const _urls={
     _info: "https://api.github.com/orgs/MACS-DTU",
     _repos: "https://api.github.com/orgs/MACS-DTU/repos",
-    _members: "https://api.github.com/orgs/MACS-DTU/public_members"
+    _members: "https://api.github.com/orgs/MACS-DTU/public_members",
+    _sheets_url: "https://docs.google.com/spreadsheets/d/1dttEoM1l_W2Yatk1TT2QxtNHx_wnBzNd3pq2_yszBAs/edit?usp=sharing"
 };
 
 // http helper function!
---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { get, _urls } from './constant.js';
+
+const route_name=window.location.pathname;
+
+const fetch_data=async () => {
+    // const res=await fetch(_urls._sheets_url);
+    // let data= await res.clone().json();
+    // let el = document.getElementById("sachinduhan");
+    // el.innerHTML += data;
+    // console.log(data);
+};
+
+fetch_data();
